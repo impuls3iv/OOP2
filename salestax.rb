56a173c0c63c14803d9e906b377a1b850156f04f
Sam <sammy.unterman@gmail.com>
2016-06-03 13:51:36 -0400

    added puts and gets

--- salestax.rb
+++ salestax.rb
@@ -1,5 +1,16 @@
 class Item
 
+ attr_accessor :prod_name :prod_type :imported #methods to do @name = name
+
+ def initialize(prod_name, prod_type)
+   @prod_name = prod_name
+   @prod_type = prod_type
+ end
+
+ def type
+
+ end
+
 end
 
 
@@ -7,3 +18,12 @@ end
 class Receipt < Item
 
 end
+
+puts "What is the name of your first item"
+item = gets.chomp!
+
+puts "What type of item do you have (book, food, medical, else)"
+type = gets.chomp!
+
+puts "Is your item imported?"
+import = gets.chomp!
